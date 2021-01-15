9f73da701ff39729339798e0f73823ca9dfffcde
thorly <leiyuan1229@outlook.com>
2021-01-15 15:39:02 +0800

    improvement

--- GAMMA/Phase-stacking/diff_tab.py
+++ GAMMA/Phase-stacking/diff_tab.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 #####################################################
-# Write diff_tab for Stacking using GAMMA           #
+# write diff_tab for Phase-Stacking using GAMMA     #
 # Author: Yuan Lei, 2020                            #
 #####################################################
-import os
-import glob
+import argparse
 import datetime
+import glob
+import os
 import re
+import sys
+
+EXAMPLE = """Example:
+  ./diff_tab.py -i /media/ly/stacking -e diff.int.unw -o .
+"""
+
+
+def cmdline_parser():
+    parser = argparse.ArgumentParser(
+        description='write diff_tab for Phase-Stacking using GAMMA.',
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog=EXAMPLE)
+
+    parser.add_argument('-i', dest='ifg_dir', help='path of ifg directory.')
+    parser.add_argument('-e',
+                        dest='extension',
+                        help='filename extension of unwrapping file.')
+    parser.add_argument('-o',
+                        dest='out_dir',
+                        help='path of directory saving diff_tab.')
+
+    inps = parser.parse_args()
+
+    return inps
 
 
 def get_baseline(unw):
@@ -19,14 +44,37 @@ def get_baseline(unw):
     return baseline
 
 
-def write_diff_tab(ifg_dir, diff_tab_path):
-    unws = glob.glob(os.path.join(ifg_dir, '*/*.unw'))
-    with open(diff_tab_path, 'w+') as f:
+def get_unws(ifg_dir, extension):
+    unws = glob.glob(os.path.join(ifg_dir, '*/*.' + extension))
+    return unws
+
+
+def write_diff_tab(unws, out_dir):
+    diff_tab = os.path.join(out_dir, 'diff_tab')
+    print('writing data to {}'.format(diff_tab))
+    with open(diff_tab, 'w+') as f:
         for unw in unws:
             baseline = get_baseline(unw)
             f.write(f"{unw} {baseline}\n")
+    print('done, enjot it.')
 
 
-ifg_dir = '/media/ly/文件/gamma_stack/gamma_stack/interferograms'
-diff_tab_path = 'diff_tab'
-write_diff_tab(ifg_dir, diff_tab_path)
+def main():
+    # get inputs
+    inps = cmdline_parser()
+    ifg_dir = os.path.abspath(inps.ifg_dir)
+    extension = inps.extension
+    out_dir = os.path.abspath(inps.out_dir)
+    # get unws
+    unws = get_unws(ifg_dir, extension)
+    if len(unws) == 0:
+        print('no unws in {}'.format(ifg_dir))
+        sys.exit()
+    # write diff_tab
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+    write_diff_tab(unws, out_dir)
+
+
+if __name__ == "__main__":
+    main()
