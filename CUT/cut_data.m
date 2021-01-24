7ebe52e5a67d56cd7f1b6285a647a99c568d2963
“thorly_lat_win” <leiyuan1229@outlook.com>
2021-01-24 20:27:14 +0800

    general improvement

--- CUT/cut_data.m
+++ CUT/cut_data.m
@@ -1,31 +1,29 @@
-clear all;
-close all;
-clc;
+function [] = cut_data( vel_in, lonlat, vel_out )
+%cut data using longitude and latitued
 
-disp=load('mudui3_SBAS_T.txt');
-num=disp(:,1); 
-lon=disp(:,2);
-lat=disp(:,3);
-def=disp(:,4);
+data=load(vel_in);
+lon=data(:,1);
+lat=data(:,2);
+vel=data(:,3);
 
-lon_lat=load('T_line.txt');
+lon_lat=load(lonlat);
 t_lon=lon_lat(:,1);
 t_lat=lon_lat(:,2);
 
 BWout=inpolygon(lon,lat,t_lon,t_lat);
 numOfZero=find(BWout==0);
 
-clear t_lon;
-clear t_lat;
-
 lon(numOfZero)=NaN;
 lat(numOfZero)=NaN;
 
-outInfo=[num(:) lon(:) lat(:) def(:)];
-numNotNaN=find(~isnan(outInfo(:,2)));
+outInfo=[lon(:) lat(:) vel(:)];
+numNotNaN= ~isnan(outInfo(:,2));
 outInfo=outInfo(numNotNaN,:);
-% save mudui21_SBAS_TT.txt outInfo -ascii
 
-fid=fopen('mudui3_SBAS_TT.txt','wt');
-fprintf(fid,'%d\t%f\t%f\t%f\n',outInfo');
+fid=fopen(vel_out,'wt');
+fprintf(fid,'%f\t%f\t%f\n',outInfo');
 fclose(fid);
+
+clear t_lon;clear t_lat;clear lon;clear lat;clear vel;clear data;
+
+end
